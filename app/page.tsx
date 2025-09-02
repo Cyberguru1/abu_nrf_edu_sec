4fdea6f2aa766fe7c53e8b076a68ed64cc86ecf1
cyberguru <hamzacypher34@gmail.com>
2025-09-02 02:40:45 -0500

    chore: fix connected name

--- app/page.tsx
+++ app/page.tsx
@@ -1285,7 +1285,7 @@ export default class VehicleSecuritySystem extends Component<{}, VehicleSecurity
           ? 'bg-green-100 text-green-800'
           : 'bg-red-100 text-red-800'
         }`}>
-        {this.state.webSocketConnected ? '🟢 Connected' : '🔴 Disconnected'}
+        {this.state.webSocketConnected ? '🟢' : '🔴'}
       </div>
     );
   }
